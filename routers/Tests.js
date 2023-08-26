dc7b142eaccf1c3822203f0031afc98172225564
Shoaibsolanki73 <118595013+Shoaibsolanki73@users.noreply.github.com>
2023-08-26 12:35:25 +0530

    public

--- routers/Tests.js
+++ routers/Tests.js
@@ -7,7 +7,7 @@ const test = require("../models/Test");
 const multer = require('multer')
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads')
+    cb(null, './public')
   },
   filename: function (req, file, cb){
     cb(null, file.originalname)
